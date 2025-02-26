7e3d000845584461d7ff8b07fdd7b5c7880f4387
Juancarlos <jcramos20029109@gmail.com>
2025-02-25 21:36:07 -0800

    Ready To Grade

--- tests/SumNumbersTest.java
+++ tests/SumNumbersTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SumNumbersTest {
+    @Test
+    void sumIsEvenTest1() {
+        SumNumbers sumEvenTest1 = new SumNumbers();
+        double result1 = sumEvenTest1.sumIsEven(1,1);
+        assertEquals(1, result1);
+    }
+    @Test
+    void sumIsEvenTest2() {
+        SumNumbers sumEvenTest2 = new SumNumbers();
+        double result2 = sumEvenTest2.sumIsEven(1,2);
+        assertEquals(0, result2);
+    }
 }
--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TipCalculatorTest {
+    @Test
+    void calculatorTest1() {
+        TipCalculator calculatorTest1 = new TipCalculator();
+        double result1 = calculatorTest1.calculateTip(100.00, 20);
+        assertEquals(20, result1);
+    }
+    @Test
+    void calculatorTest2() {
+        TipCalculator calculatorTest2 = new TipCalculator();
+        double result2 = calculatorTest2.calculateTip(1000.00, 20);
+        assertEquals(200, result2);
+    }
 }
